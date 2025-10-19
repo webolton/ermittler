45c5212431a5a324663cf6020f2144fd1601dfc1
William Bolton <wbolton@Williams-MacBook-Pro.local>
2025-10-19 15:18:39 -0400

    Add seeds for creating users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
-20.times do |time|
-  User.create
+require 'faker'
+
+User.destroy_all
+
+50.times do |time|
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+  User.create!(
+    first_name:,
+    last_name:,
+    email: "#{first_name}_#{last_name}@ermittler.test.com",
+    oid: SecureRandom.uuid,
+    username: "#{first_name.first}#{last_name[0..3]}#{time}02".downcase
+  )
 end
