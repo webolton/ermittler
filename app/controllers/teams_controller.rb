a2c59b94f3e4ca840e6b859beaf43ab12da560a7
William Bolton <wbolton@Williams-MacBook-Pro.local>
2025-10-19 15:37:49 -0400

    Add relationship for teams / users + rubocop fixes

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class TeamsController < ApplicationController
-  before_action :set_team, only: %i[ show edit update destroy ]
+  before_action :set_team, only: %i[show edit update destroy]
 
   # GET /teams or /teams.json
   def index
@@ -7,8 +9,7 @@ class TeamsController < ApplicationController
   end
 
   # GET /teams/1 or /teams/1.json
-  def show
-  end
+  def show; end
 
   # GET /teams/new
   def new
@@ -16,8 +17,7 @@ class TeamsController < ApplicationController
   end
 
   # GET /teams/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /teams or /teams.json
   def create
@@ -25,7 +25,7 @@ class TeamsController < ApplicationController
 
     respond_to do |format|
       if @team.save
-        format.html { redirect_to @team, notice: "Team was successfully created." }
+        format.html { redirect_to @team, notice: 'Team was successfully created.' }
         format.json { render :show, status: :created, location: @team }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class TeamsController < ApplicationController
   def update
     respond_to do |format|
       if @team.update(team_params)
-        format.html { redirect_to @team, notice: "Team was successfully updated.", status: :see_other }
+        format.html { redirect_to @team, notice: 'Team was successfully updated.', status: :see_other }
         format.json { render :show, status: :ok, location: @team }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +52,20 @@ class TeamsController < ApplicationController
     @team.destroy!
 
     respond_to do |format|
-      format.html { redirect_to teams_path, notice: "Team was successfully destroyed.", status: :see_other }
+      format.html { redirect_to teams_path, notice: 'Team was successfully destroyed.', status: :see_other }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_team
-      @team = Team.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def team_params
-      params.expect(team: [ :email, :name ])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_team
+    @team = Team.find(params.expect(:id))
+  end
+
+  # Only allow a list of trusted parameters through.
+  def team_params
+    params.expect(team: %i[email name])
+  end
 end
