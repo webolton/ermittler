8a76abc741c81bc126f5b4c968de2c5a5342995d
William Bolton <wbolton@Williams-MacBook-Pro.local>
2025-10-20 08:24:46 -0400

    Set up jquery and popper

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+import "jquery";
+import "@popperjs/core"
 import "@hotwired/turbo-rails"
 import "controllers"
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
+
+pin "jquery", to: 'https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js'
+pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/lib/index.js"
+
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
